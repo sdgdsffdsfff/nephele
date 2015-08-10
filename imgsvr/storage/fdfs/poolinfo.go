69293e065cd7a5a9de37e4bf1de851120aff6f6c
zhyang <zhyang@Ctrip.com>
2015-08-10 17:30:34 +0800

    fdfs client bug fix

--- imgsvr/storage/fdfs/poolinfo.go
+++ imgsvr/storage/fdfs/poolinfo.go
@@ -21,15 +21,16 @@ func (this *poolInfo) makeConn() (net.Conn, error) {
 	conn, err := net.DialTimeout("tcp", addr, 30*time.Second)
 	if err != nil {
 		event := Cat.NewEvent("DialFdfs", "Fail")
+		event.AddData("addr", addr)
 		event.AddData("detail", err.Error())
 		event.SetStatus("ERROR")
 		event.Complete()
 		return nil, err
 	}
 	event := Cat.NewEvent("DialFdfs", "Success")
+	event.AddData("addr", addr)
 	event.SetStatus("0")
 	event.Complete()
-	return nil, err
 	return conn, nil
 }
 
